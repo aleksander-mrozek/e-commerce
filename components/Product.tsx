0e75a523d7047e3902c7196cd1b6dc3cfeeacae0
Aleksander Mrożek <mrozek.aleksander@gmail.com>
2022-10-18 20:15:09 +0200

    perf(metadata):seo optimization with next-seo

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from "react-markdown";
+import { NextSeo } from "next-seo";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -22,6 +23,24 @@ export const ProductDetails = ({ data }: ProductProps) => {
   return (
     <>
       <div className="bg-white p-4">
+        <NextSeo
+          title={data.title}
+          description={data.description}
+          canonical={`https://naszsklep-api.vercel.app/api/products/${data.id}`}
+          openGraph={{
+            url: `https://naszsklep-api.vercel.app/api/products/${data.id}`,
+            title: data.title,
+            description: data.description,
+            images: [
+              {
+                url: data.thumbnailUrl,
+                alt: data.thumbnailAlt,
+                type: "image/jpeg",
+              },
+            ],
+            site_name: "E-commerce",
+          }}
+        />
         <Image
           src={data.thumbnailUrl}
           alt={data.thumbnailAlt}
