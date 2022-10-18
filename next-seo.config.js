3b1d05e8965b511d19f8d75306583f3c163b8c62
Aleksander Mrożek <mrozek.aleksander@gmail.com>
2022-10-18 21:28:22 +0200

    perf(seo):add component DefaultSeo

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const title = "E-commerce";
+const description = "E-commerce website. Created with modern frontend stack.";
+
+const nextSeoConfig = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    site_name: "E-commerce",
+  },
+};
+
+export default nextSeoConfig;
