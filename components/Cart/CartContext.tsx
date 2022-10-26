062b286fa76846ffab0034bf5e913291583b3fa5
Aleksander Mrożek <100879231+aleksander-mrozek@users.noreply.github.com>
2022-10-26 20:59:23 +0200

    Delete item cart bug (#2)
    * fix(cart):bug in removeItemFromCart and localStorage
    
    * fix(cart):remove bug from removeItemFromCart and adjust localStorage to React 18

--- components/Cart/CartContext.tsx
+++ components/Cart/CartContext.tsx
@@ -1,6 +1,15 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-interface CartItem {
+import { getCartItemsFromStorage, setCartItemsInStorage } from "./CartStorage";
+
+export interface CartItem {
+  readonly id: number;
   readonly price: number;
   readonly title: string;
   readonly count: number;
@@ -9,6 +18,7 @@ interface CartItem {
 interface CartState {
   readonly items: readonly CartItem[];
   readonly addItemToCart: (item: CartItem) => void;
+  readonly removeItemFromCart: (id: CartItem["id"]) => void;
 }
 
 export const CartStateContext = createContext<CartState | null>(null);
@@ -18,28 +28,55 @@ export const CartStateContextProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[] | undefined>(undefined);
+
+  useEffect(() => {
+    setCartItems(getCartItemsFromStorage());
+  }, []);
+
+  useEffect(() => {
+    if (cartItems === undefined) {
+      return;
+    }
+    setCartItemsInStorage(cartItems);
+  }, [cartItems]);
+
   return (
     <CartStateContext.Provider
       value={{
-        items: cartItems,
+        items: cartItems || [],
         addItemToCart: (item) => {
-          setCartItems((prevState) => {
-            const existingItem = prevState.find(
-              (existingItem) => existingItem.title === item.title
-            );
+          setCartItems((prevState = []) => {
+            const existingItem = prevState.find((el) => el.id === item.id);
             if (!existingItem) {
               return [...prevState, item];
             }
 
-            return prevState.map((existingItem) => {
-              if (existingItem.title === item.title) {
+            return prevState.map((el) => {
+              if (el.id === item.id) {
                 return {
-                  ...existingItem,
-                  count: existingItem.count + 1,
+                  ...el,
+                  count: el.count + 1,
                 };
               }
-              return existingItem;
+              return el;
+            });
+          });
+        },
+        removeItemFromCart: (id) => {
+          setCartItems((prevState = []) => {
+            const existingItem = prevState.find((el) => el.id === id);
+            if (existingItem && existingItem.count <= 1) {
+              return prevState.filter((el) => el.id !== id);
+            }
+            return prevState.map((el) => {
+              if (el.id === id) {
+                return {
+                  ...el,
+                  count: el.count - 1,
+                };
+              }
+              return el;
             });
           });
         },
---
+++ components/Cart/CartStorage.tsx
@@ -0,0 +1,19 @@
+import { CartItem } from "./CartContext";
+
+export const getCartItemsFromStorage = () => {
+  const itemsFromLocalStorage = localStorage.getItem("E-commerce-key");
+  if (!itemsFromLocalStorage) {
+    return [];
+  }
+  try {
+    const items = JSON.parse(itemsFromLocalStorage);
+    return items;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
+export const setCartItemsInStorage = (cartItems: CartItem[]) => {
+  localStorage.setItem("E-commerce-key", JSON.stringify(cartItems));
+};
