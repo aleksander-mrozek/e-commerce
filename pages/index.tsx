8a52ad2bb9d9cbd5302f07a21423680bd04b8fb1
Aleksander Mrożek <mrozek.aleksander@gmail.com>
2022-11-16 22:16:21 +0100

    feat(reviews):add GraphQL mutation for a sample review

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
-import { gql, useQuery } from "@apollo/client";
-
 import { Main } from "../components/Main";
+import { useCreateProductReviewMutation } from "../generated/graphql";
 
 const Home = () => {
-  const { loading, error, data } = useQuery(gql`
-    query GetProductsList {
-      products {
-        id
-        slug
-        name
-        price
-      }
-    }
-  `);
-  if (loading) return <Main>Loading...</Main>;
-  if (error) return <Main>{JSON.stringify(error)}</Main>;
+  const [createReview, { data, loading, error }] =
+    useCreateProductReviewMutation();
+
+  const addReview = () =>
+    createReview({
+      variables: {
+        review: {
+          headline: "789 bad product",
+          name: "Aleksander M",
+          email: "example123@gmail.com",
+          content: "This is the most disgusting thing I ever bought...",
+          rating: 1,
+        },
+      },
+    });
+
   return (
     <Main>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <button onClick={addReview} type="button">
+        Add review
+      </button>
+      {loading && <div className="text-red-500">Loading...</div>}
+      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </Main>
   );
 };
